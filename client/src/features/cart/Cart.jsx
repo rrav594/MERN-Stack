3623fc2354db3504946907626864ab7cf318edbf
Dev <dev@Ravis-iMac.local>
2024-01-27 02:36:21 +0530

    create and implement loading element

--- client/src/features/cart/Cart.jsx
+++ client/src/features/cart/Cart.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Cart() {
-  return <div>Cart</div>;
+  return (
+    <div>
+      Cart
+      <Link to="/menu">Got to Menu</Link>
+    </div>
+  );
 }
 
 export default Cart;
