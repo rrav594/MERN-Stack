93af8d4b16d877272f1bb1f41d7c35ab01c1320f
Dev <dev@Ravis-iMac.local>
2024-01-27 04:05:56 +0530

    create a new order using react-router-dom FORM method

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,9 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
 import Cart from "./features/cart/Cart";
 import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
@@ -23,7 +25,11 @@ const router = createBrowserRouter([
         errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
-      { path: "/order/new", element: <CreateOrder /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
       {
         path: "/order/:orderId",
         element: <Order />,
--- client/src/features/order/CreateOrder.jsx
+++ client/src/features/order/CreateOrder.jsx
@@ -1,5 +1,57 @@
+import { Form, redirect } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  console.log(data);
+  const order = {
+    ...data,
+    // cart: JSON.parse(data.cart),
+    priority: data.priority === "on",
+  };
+  console.log(order);
+  const newOrder = await createOrder(order);
+
+  return redirect(`/order/${newOrder.id}`);
+}
+
 function CreateOrder() {
-  return <div>Createorder</div>;
+  return (
+    <div>
+      <h1>Ready to order? Place here...</h1>
+      <Form method="POST" action="">
+        <div>
+          <label>First Name</label>
+          <div>
+            <input type="text" name="customer" required />
+          </div>
+        </div>
+        <div>
+          <label>Phone Number</label>
+          <div>
+            <input type="tel" name="phone" required />
+          </div>
+        </div>
+        <div>
+          <label>Address</label>
+          <div>
+            <input type="text" name="address" required />
+          </div>
+        </div>
+        <div>
+          <input id="priority" type="checkbox" name="priority" id="priority" />
+
+          <label for="priority">Want it delivered soon</label>
+        </div>
+        <hr />
+        <div>
+          {/* <input type="hidden" name="cart" value={JSON.stringify(cart)} /> */}
+          <button>Order Now</button>
+        </div>
+      </Form>
+    </div>
+  );
 }
 
 export default CreateOrder;
